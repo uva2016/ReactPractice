8a4608d7fbaa53b33a05809428e290023981cffc
Graham Turner <grahamturner1993@gmail.com>
2018-07-10 23:50:04 -0400

    main game components

--- GuessNine.js
+++ GuessNine.js
@@ -1,18 +1,65 @@
+const Stars = (props) => {
+  return (
+    <div className="col-5">
+      <i className="fa fa-star"></i>
+      <i className="fa fa-star"></i>
+      <i className="fa fa-star"></i>
+      <i className="fa fa-star"></i>
+    </div>
+  );
+};
+
+const Button = (props) => {
+  return (
+    <div className="col-2">
+      <button>=</button>
+    </div>
+  );
+};
+
+const Answer = (props) => {
+  return (
+    <div className="col-5">
+      ...
+    </div>
+  );
+};
+
+const Numbers = (props) => {
+  return (
+    <div className="card text-center">
+      <div>
+        <span>1</span>
+        <span className="selected">2</span>
+        <span className="used">3</span>
+      </div>
+    </div>
+  );
+};
+
 class Game extends React.Component {
-	render() {
-  	return (
-    	<div>
-      	<h3>Play Nine</h3>
+  render() {
+    return (
+      <div className="container">
+        <h3>Play Nine</h3>
+        <hr />
+        <div className="row">
+          <Stars />
+          <Button />
+          <Answer />
+        </div>
+        <br />
+        <Numbers />
       </div>
     );
   }
 }
 
 class App extends React.Component {
-	render() {
-  	return (
-    	<div>
-      	<Game />
+  render() {
+    return (
+      <div>
+        <Game />
       </div>
     );
   }
