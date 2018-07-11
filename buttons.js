678c010edede415ec3abf8632ed9941c7f44a5dc
William Turner <wturner@mbp-gturner.local>
2018-07-10 23:37:22 -0400

    added in more buttons with different values

--- buttons.js
+++ buttons.js
@@ -1,19 +1,43 @@
 class Button extends React.Component {
-  state = { counter: 0 };
-  
   handleClick = () => {
-    this.setState((prevState) => ({
-      counter: prevState.counter + 1
-    }));
+    this.props.onClickFunction(this.props.incrementValue);
   };
-  
+
   render() {
     return (
       <button onClick={this.handleClick}>
-        {this.state.counter}
+        +{this.props.incrementValue}
       </button>
     );
   }
 }
 
-ReactDOM.render(<Button />, mountNode);
+const Result = (props) => {
+  return (
+    <div>{props.counter}</div>
+  );
+};
+
+class App extends React.Component {
+  state = { counter: 0 };
+
+  incrementCounter = (incrementValue) => {
+    this.setState((prevState) => ({
+      counter: prevState.counter + incrementValue
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <Button incrementValue={1} onClickFunction={this.incrementCounter} />
+        <Button incrementValue={5} onClickFunction={this.incrementCounter} />
+        <Button incrementValue={10} onClickFunction={this.incrementCounter} />
+        <Button incrementValue={100} onClickFunction={this.incrementCounter} />
+        <Result counter={this.state.counter} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, mountNode);
