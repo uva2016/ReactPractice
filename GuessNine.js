9c8dc55bb48b8ffdde6ab8122a0596c5a464997a
Graham Turner <grahamturner1993@gmail.com>
2018-07-10 23:55:57 -0400

    Selection of numbers functional

--- GuessNine.js
+++ GuessNine.js
@@ -1,9 +1,7 @@
 const Stars = (props) => {
-  const numberOfStars = 1 + Math.floor(Math.random()*9);
-
   return (
     <div className="col-5">
-      {_.range(numberOfStars).map(i =>
+      {_.range(props.numberOfStars).map(i =>
         <i key={i} className="fa fa-star"></i>
       )}
     </div>
@@ -13,7 +11,9 @@ const Stars = (props) => {
 const Button = (props) => {
   return (
     <div className="col-2">
-      <button>=</button>
+      <button className="btn" disabled={props.selectedNumbers.length === 0}>
+        =
+      </button>
     </div>
   );
 };
@@ -21,38 +21,69 @@ const Button = (props) => {
 const Answer = (props) => {
   return (
     <div className="col-5">
-      <span>5</span>
-      <span>6</span>
+      {props.selectedNumbers.map((number, i) =>
+        <span key={i} onClick={() => props.unselectNumber(number)}>
+          {number}
+        </span>
+      )}
     </div>
   );
 };
 
 const Numbers = (props) => {
+  const numberClassName = (number) => {
+    if (props.selectedNumbers.indexOf(number) >= 0) {
+      return 'selected';
+    }   
+  };
   return (
     <div className="card text-center">
       <div>
         {Numbers.list.map((number, i) =>
-          <span key={i}>{number}</span>
+          <span key={i} className={numberClassName(number)}
+                onClick={() => props.selectNumber(number)}>
+            {number}
+          </span>
         )}
       </div>
     </div>
   );
 };
+
 Numbers.list = _.range(1, 10);
 
 class Game extends React.Component {
+  state = {
+    selectedNumbers: [],
+    randomNumberOfStars: 1 + Math.floor(Math.random()*9),
+  };
+  selectNumber = (clickedNumber) => {
+    if (this.state.selectedNumbers.indexOf(clickedNumber) >= 0) { return; }
+    this.setState(prevState => ({
+      selectedNumbers: prevState.selectedNumbers.concat(clickedNumber)
+    }));
+  };
+  unselectNumber = (clickedNumber) => {
+    this.setState(prevState => ({
+      selectedNumbers: prevState.selectedNumbers
+                                .filter(number => number !== clickedNumber)
+    }));
+  };
   render() {
+    const { selectedNumbers, randomNumberOfStars } = this.state;
     return (
       <div className="container">
         <h3>Play Nine</h3>
         <hr />
         <div className="row">
-          <Stars />
-          <Button />
-          <Answer />
+          <Stars numberOfStars={randomNumberOfStars} />
+          <Button selectedNumbers={selectedNumbers} />
+          <Answer selectedNumbers={selectedNumbers}
+                  unselectNumber={this.unselectNumber} />
         </div>
         <br />
-        <Numbers />
+        <Numbers selectedNumbers={selectedNumbers}
+                 selectNumber={this.selectNumber} />
       </div>
     );
   }
